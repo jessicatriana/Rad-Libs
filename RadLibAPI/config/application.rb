d794c05c24bfea326f9bce61671c25407e47d44b
Dane-Dawson <Dane.P.Dawson@gmail.com>
2020-03-25 14:41:53 -0600

    fixed random * that broken file seed

--- RadLibAPI/config/application.rb
+++ RadLibAPI/config/application.rb
@@ -18,7 +18,7 @@ module RadLibAPI
     config.middleware.insert_before 0, Rack::Cors do
       allow do
  
-        origins '*'*
+        origins '*'
         resource '*', headers: :any, methods: [:get, :post]
       end
     end
